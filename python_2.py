e1c518db82e0c3c66583cbe1f3e28a3cc7816f2d
falseych <597821319@qq.com>
2018-07-28 10:20:34 +0800

    REMOVE

--- python_2.py
+++
@@ -1,7 +0,0 @@
-print "I have some chickens",5+3
-print "Hens",5
-print "Roosters",3
-print "Do I have more hens than roosters?",5>3
-print "Do I have no less than 2 roosters?",3>=2
-print "5==5\t5==3\t",5==5,"\t","5==3"
-print "5%3=",5%3,"\t5/3=",5/3,"\t5.0/3=",5.0/3
